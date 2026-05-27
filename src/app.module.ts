30020d508905c1f9b3b6d7c7343bb1ab934f7a52
ArchiveDipta <archivepradipta@gmail.com>
2026-05-27 17:47:45 +0700

    fix: add AppController import and healthcheck endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
@@ -7,7 +9,6 @@ import { VouchersModule } from './modules/vouchers/vouchers.module';
 import { CartModule } from './modules/cart/cart.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { AnalyticsModule } from './modules/analytics/analytics.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -21,5 +22,6 @@ import { PrismaModule } from './prisma/prisma.module';
     OrdersModule,
     AnalyticsModule,
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
